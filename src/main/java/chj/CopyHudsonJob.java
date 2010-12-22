09b6931a317e1e9be5a9de84c0bf2bb52ed14651
ikikko <ikikko+github@gmail.com>
2010-12-23 02:17:28 +0900

    ジョブのコピーも、hudson-cliではなくAPI経由で行う

--- src/main/java/chj/CopyHudsonJob.java
+++ src/main/java/chj/CopyHudsonJob.java
@@ -1,16 +1,18 @@
 package chj;
 
-import hudson.cli.CLI;
-
 import java.awt.Desktop;
 import java.net.URI;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
@@ -20,11 +22,13 @@ import org.kohsuke.args4j.Option;
 public class CopyHudsonJob {
 
 	@Option(name = "-u", metaVar = "URL", usage = "HudsonのURL")
-	static String url = "http://192.168.233.131/hudson/";
+	static String hudsonUrl = "http://192.168.233.131/hudson/";
 	@Option(name = "-s", metaVar = "src", usage = "コピー元ジョブ")
 	static String src = "templateJob";
 	@Option(name = "-d", metaVar = "dst", usage = "コピー先ジョブ", required = true)
 	static String dst;
+
+	private static HttpClient client;
 
 	public static void main(String[] args) throws Exception {
 		// コマンドライン引数のパース
@@ -41,28 +45,56 @@ public class CopyHudsonJob {
 			System.exit(1);
 		}
 
-		// Jobのコピー
-		// TODO 既に存在する場合はエラーにする
-		CLI cli = new CLI(new URL(url));
-		cli.execute("copy-job", src, dst);
-		cli.close();
+		client = new DefaultHttpClient();
+		copyJob(src, dst);
+		enabledJob();
+		replaceJobSetting(dst);
+		client.getConnectionManager().shutdown();
 
-		// HttpClientの設定
-		String jobUrl = url + "job" + "/" + dst + "/";
-		HttpClient client = new DefaultHttpClient();
-
-		// config.xmlの取得
-		String configXmlUrl = jobUrl + "config.xml";
-		HttpGet configXmlRequest = new HttpGet(configXmlUrl);
-		HttpResponse configXmlResponse = client.execute(configXmlRequest);
-		String configXml = EntityUtils.toString(configXmlResponse.getEntity());
-
-		// ジョブの有効化
-		String enableUrl = jobUrl + "enable";
-		HttpPost enableRequest = new HttpPost(enableUrl);
-		client.execute(enableRequest);
-
-		// ジョブのWebページを開く
-		Desktop.getDesktop().browse(new URI(jobUrl));
+		Desktop.getDesktop().browse(
+				new URI(hudsonUrl + "job" + "/" + dst + "/"));
 	}
+
+	/**
+	 * ジョブをコピーする。
+	 */
+	private static void copyJob(String src, String dst) throws Exception {
+		// TODO 既に存在する場合はエラーにする
+		String requestUrl = hudsonUrl + "createItem";
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("name", dst));
+		params.add(new BasicNameValuePair("mode", "copy"));
+		params.add(new BasicNameValuePair("from", src));
+
+		HttpPost ｒequest = new HttpPost(requestUrl);
+		ｒequest.setEntity(new UrlEncodedFormEntity(params));
+		client.execute(ｒequest).getEntity().consumeContent();
+
+		System.out.println("copy job from [" + src + "] to [" + dst + "]");
+	}
+
+	/**
+	 * ジョブを有効化する。
+	 */
+	private static void enabledJob() throws Exception {
+		String requestUrl = hudsonUrl + "job" + "/" + dst + "/" + "enable";
+
+		HttpPost request = new HttpPost(requestUrl);
+		client.execute(request).getEntity().consumeContent();
+
+		System.out.println("enable job [" + dst + "]");
+	}
+
+	/**
+	 * ジョブの設定内容を置き換える。
+	 */
+	private static void replaceJobSetting(String dst) throws Exception {
+		String requestUrl = hudsonUrl + "job" + "/" + dst + "/" + "config.xml";
+
+		HttpGet request = new HttpGet(requestUrl);
+		HttpResponse response = client.execute(request);
+		String configXml = EntityUtils.toString(response.getEntity());
+		response.getEntity().consumeContent();
+	}
+
 }
